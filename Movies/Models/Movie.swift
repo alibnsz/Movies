8c3afa3fe2764855949a16e7c1ffad2d427b5f3e
Mehmet Ali Bunsuz <m.bunsuz1@gmail.com>
2025-08-20 22:00:56 +0300

    feat: implement core data models for Movie and TV Show

---
+++ Movies/Models/Movie.swift
@@ -0,0 +1,36 @@
+//
+//  Movie.swift
+//  Movies
+//
+//  Created by Mehmet Ali Bunsuz on 20.08.2025.
+//
+
+import Foundation
+
+struct TrendingMovieResponse: Codable {
+    let results: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int
+    let mediaType: String?
+    let originalName: String?
+    let originalTitle: String?
+    let posterPath: String?
+    let overview: String?
+    let voteCount: Int?
+    let releaseDate: String?
+    let voteAverage: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case mediaType = "media_type"
+        case originalName = "original_name"
+        case originalTitle = "original_title"
+        case posterPath = "poster_path"
+        case overview
+        case voteCount = "vote_count"
+        case releaseDate = "release_date"
+        case voteAverage = "vote_average"
+    }
+}
