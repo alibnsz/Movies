e4b32cb5f0fd359fa21e586c0c2722f8680039c8
Mehmet Ali Bunsuz <m.bunsuz1@gmail.com>
2025-08-25 15:08:53 +0300

    refactor(controllers): reorganize controllers into Core and General; move Home, Upcoming, Download; add Search to Core; add SearchResult to General

---
+++ Movies/Controllers/Core/SearchViewController.swift
@@ -0,0 +1,110 @@
+//
+//  SearchViewController.swift
+//  Movies
+//
+//  Created by Mehmet Ali Bunsuz on 20.08.2025.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+        
+    private let discoverTable: UITableView = {
+        
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+        
+    }()
+    
+    private let searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: SearchResultViewController())
+        controller.searchBar.placeholder = "Search for movie or a TV show..."
+        controller.searchBar.searchBarStyle = .minimal
+        return controller
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        view.backgroundColor = .systemBackground
+        view.addSubview(discoverTable)
+        discoverTable.delegate = self
+        discoverTable.dataSource = self
+        
+        navigationItem.searchController = searchController
+        navigationController?.navigationBar.tintColor = .white
+        
+        fetchDiscoverMovies()
+        
+        searchController.searchResultsUpdater = self
+    }
+    
+    private func fetchDiscoverMovies() {
+        ApiCaller.shared.getUpcomingMovies { result in
+            switch result {
+            case .success(let titles):
+                print(result)
+                self.titles = titles
+                DispatchQueue.main.async {
+                    self.discoverTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
+    }
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+            return UITableViewCell()
+        }
+        let title = titles[indexPath.row]
+        let model = TitleViewModel(titleName: title.originalName ?? title.originalTitle ?? "Unkown Title Name", posterURL: title.posterPath ?? "")
+        cell.configure(with: model)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty,
+              query.trimmingCharacters(in: .whitespaces).count >= 3,
+              let resultsController = searchController.searchResultsController as? SearchResultViewController else {
+                  return
+        }
+        
+        ApiCaller.shared.search(with: query) { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let titles):
+                    resultsController.titles = titles
+                    resultsController.searchResultCollectionView.reloadData()
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
